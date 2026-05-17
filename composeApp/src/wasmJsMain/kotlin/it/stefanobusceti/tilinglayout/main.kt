6d68800fcf98eafd75d9549226daee6b5c62530e
Stefano Busceti <stefano.busceti@gmail.com>
2026-05-17 10:38:47 +0200

    feat: wasm setup

--- composeApp/src/wasmJsMain/kotlin/it/stefanobusceti/tilinglayout/main.kt
+++ composeApp/src/wasmJsMain/kotlin/it/stefanobusceti/tilinglayout/main.kt
@@ -2,9 +2,11 @@ package it.stefanobusceti.tilinglayout
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import it.stefanobusceti.tilinglayout.di.initKoin
 import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    initKoin()
     ComposeViewport(document.body ?: return) { App() }
 }
